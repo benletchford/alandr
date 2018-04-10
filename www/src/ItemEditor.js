2a40e75ba027b927a7d9ea9ac8c8c5a370d60f62
Ben Letchford <contact@benl.com.au>
2018-04-10 20:51:42 +1000

    table

--- www/src/ItemEditor.js
+++ www/src/ItemEditor.js
@@ -9,22 +9,61 @@ import {
   DialogFooterButton,
   DialogBackdrop
 } from 'rmwc/Dialog';
+import { TextField, TextFieldIcon, TextFieldHelperText } from 'rmwc/TextField';
 import { view } from 'react-easy-state'
+import { Grid, GridCell } from 'rmwc/Grid';
+import { Icon } from 'rmwc/Icon';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryText,
+  ListItemGraphic,
+  ListItemMeta,
+  ListDivider
+} from 'rmwc/List';
+import { Checkbox } from 'rmwc/Checkbox';
 
+import './ItemEditor.css';
 import store from './Store'
 
 class ItemEditor extends Component {
   render() {
+    var items = [];
+    for (var i=0;i<store.data.items.length;i++) {
+        items.push(
+          <tr key={i}>
+            <td><Checkbox></Checkbox></td>
+            <td><TextField fullwidth value={store.data.items[i].href}/></td>
+            <td><TextField fullwidth value={store.data.items[i].name}/></td>
+          </tr>
+        );
+    }
+
     return (
       <Dialog
         open={store.app.editorDialogOpen}
         onClose={() => store.app.editorDialogOpen = false}
+        className="item-editor"
       >
         <DialogSurface>
           <DialogHeader>
             <DialogHeaderTitle>Link Editor</DialogHeaderTitle>
           </DialogHeader>
-          <DialogBody>This is a standard dialog.</DialogBody>
+          <DialogBody>
+            <table style={{width: 100+'%'}}>
+              <thead>
+                <tr>
+                  <td>Delete</td>
+                  <td>Link Source</td>
+                  <td>Link Name</td>
+                </tr>
+              </thead>
+              <tbody>
+                {items}
+              </tbody>
+            </table>
+          </DialogBody>
           <DialogFooter>
               <DialogFooterButton cancel>Cancel</DialogFooterButton>
               <DialogFooterButton accept>Save</DialogFooterButton>
